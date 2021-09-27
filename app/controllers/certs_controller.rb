6e37a333375c707b282bcb663d885dd53f7aa358
Loman Edwin <edwin@nouveta.tech>
2021-09-27 12:45:27 +0300

    downgraded the version from 3.0.2 to 2.6.0

--- app/controllers/certs_controller.rb
+++ app/controllers/certs_controller.rb
@@ -1,73 +1,67 @@
 class CertsController < ApplicationController
-    before_action :authorize_request, except: %i[create printcert index show]
-    before_action :find_cert, except: %i[create index printcert]
-    before_action :pcert, only: [:printcert]
-    # only: [:show, :update, :destroy]
+  before_action :authorize_request, except: %i[create printcert index show]
+  before_action :find_cert, except: %i[create index printcert]
+  # only: [:show, :update, :destroy]
 
 
-     # GET /certs
+  # GET /certs
   def index
     @certs = Cert.all
-    render json: { status: '200', messagge: 'Certs found', data: @certs},status: :ok
+    render json: { status: '200', message: 'Certs found', data: @certs }, status: :ok
   end
 
-#   print cert
+  #   print cert
 
-def printcert
-    # @user = User.find_by_username!(params[:_username])
-    # user = User.where('username = ? or email = ?', params[:text], params[:text]).first
-    # @pcert = Cert.where('id_number = ?', params[:text]).first
-    # @pcert = Cert.find_by_id_number(params[:id_number])
+  def printcert
+    @cert = Cert.find_by_id_number(params[:id_number])
+    render json: { status: '200', message: 'Certs found', data: @cert }, status: :ok
 
-    render json: { status: '200', messagge: 'Certs found', data: @cert},status: :ok
-
-end
+  end
 
   # GET /cert/{id}
   def show
-    render json: { status: '200', messagge: 'Certs found', data: @cert},status: :ok
+    render json: { status: '200', messagge: 'Certs found', data: @cert }, status: :ok
   end
 
   # POST /cert
   def create
     @cert = Cert.new(cert_params)
     if @cert.save
-      render json: { status: :created, messagge: 'Cert created successfully', data: @cert},status: :created
+      render json: { status: :created, message: 'Cert created successfully', data: @cert }, status: :created
     else
-      render json: { errors: @cert.errors.full_messages },status: :unprocessable_entity
+      render json: { errors: @cert.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
-
-   # PUT /cert/{id}
-   def update
+  # PUT /cert/{id}
+  def update
     unless @cert.update(cert_params)
       render json: { errors: @cert.errors.full_messages },
              status: :unprocessable_entity
     end
-  end
+ end
 
   # DELETE /cert/{username}
   def destroy
     @cert.destroy
   end
 
-    private
-        def find_cert
-           @cert = Cert.find(params[:id])
-          rescue ActiveRecord::RecordNotFound
-            render json: { errors: 'Cert not found' }, status: :not_found
-        end
+  private
+  def find_cert
+    @cert = Cert.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: 'Cert not found' }, status: :not_found
+  end
 
-        def pcert
-            @cert = Cert.find_by_id_number(params[:id_number])
-           rescue ActiveRecord::RecordNotFound
-             render json: { errors: 'No cert found' }, status: :not_found
-         end
+  def pcert
+    @cert = Cert.find_by_id_number(params[:id_number])
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: 'No cert found' }, status: :not_found
+   end
 
-    def cert_params
-        params.permit(
-          :full_name, :id_number, :company_name, :business_id, :examined_at, :lab_ref_number, :expiry_date
-        )
-      end
+  def cert_params
+    params.permit(
+      :full_name, :id_number, :company_name, :business_id, :examined_at, :lab_ref_number, :expiry_date
+    )
+    end
 end
