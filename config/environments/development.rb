2994530ac827ff0380fc7d42b1508282ee786de4
Loman Edwin <edwin@nouveta.tech>
2021-10-04 13:36:15 +0300

    added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,11 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+    
+
+  config.hosts << "https://afya.biller.co.ke" # Whitelist one hostname
+  config.hosts << /application\.local\Z/ # Whitelist a test domain
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
