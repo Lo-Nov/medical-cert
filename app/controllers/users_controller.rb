30811b864d00529bac1ad35663c37614fd075826
Loman Edwin <edwin@nouveta.tech>
2021-10-04 14:54:41 +0300

    added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :authorize_request, except: %i[create]
+    before_action :authorize_request, except: %i[create index]
     before_action :find_user, except: %i[create index]
 
 
