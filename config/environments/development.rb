abf91f1943576ec87cec1547bbfce5ec9e2bec01
Loman Edwin <edwin@nouveta.tech>
2021-10-08 12:15:19 +0300

    first commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
     
-  config.hosts << "afya.biller.co.ke"# Whitelist one hostname
+  config.hosts << "afya.revenuesure.co.ke"# Whitelist one hostname
   # config.hosts << /application\.local\Z/ # Whitelist a test domain
 
   # Settings specified here will take precedence over those in config/application.rb.
